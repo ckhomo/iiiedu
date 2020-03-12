b41d5faa18177658a9d9fe797e223bdd3cd720a4
ckhomo <ck9030311@gmail.com>
2020-03-12 14:30:47 +0800

    0312

--- jq_class/Lab_AJAX/Lab_AJAX_0.php
+++ jq_class/Lab_AJAX/Lab_AJAX_0.php
@@ -3,11 +3,12 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script src="jquery.min.js"></script>
 </head>
 <body>
 
 <!--
- 
+
 畫面左右各有一個下拉式選單，
 左邊的下拉式選單若選擇  A 這項時，右邉的下拉式選單要改成 A01, A02, A03, A04, A05;
 左邊的下拉式選單若改選  B 這項時，右邉的下拉式選單要改成 B01, B02, B03, B04, B05;
@@ -20,14 +21,19 @@
 			<option value="0">A</option>
 			<option value="1">B</option>
 			<option value="2">C</option>
-		</select>&nbsp;|&nbsp; 
+		</select>&nbsp;|&nbsp;
 		<select name="letterNumber" id="letterNumber">
 			<option value="0">A01</option>
 			<option value="1">B02</option>
 			<option value="2">C03</option>
-		</select> 
-		<input type="submit" value="OK" /> 
+		</select>
+		<input type="submit" value="OK" />
 	</form>
+	<div id="debug"></div>
+<script>
+	$('#debug').text('Haxyn!');
+
+</script>
 
 </body>
 </html>
